f9182c0d79deba2b17997b396b317518ce93938a
dileep-hegde <dileephegde96@gmail.com>
2026-02-20 20:36:31 +0530

    update the site content

---
+++ docs/docs/intro.md
@@ -0,0 +1,7 @@
+---
+sidebar_position: 1
+---
+
+# Intro
+
+Welcome to the Dev Reference! This is a collection of resources and guides for developers. Whether you're a beginner or an experienced developer, you'll find something useful here.
---
+++ docs/docusaurus.config.ts
@@ -0,0 +1,76 @@
+import { themes as prismThemes } from "prism-react-renderer";
+import type { Config } from "@docusaurus/types";
+import type * as Preset from "@docusaurus/preset-classic";
+
+// This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
+
+const config: Config = {
+  title: "Dev Reference",
+  tagline: "A reference for personal use, by a developer.",
+  favicon: "img/favicon.ico",
+
+  // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
+  future: {
+    v4: true, // Improve compatibility with the upcoming Docusaurus v4
+  },
+
+  // Set the production url of your site here
+  url: "https://devreference.in",
+  baseUrl: "/",
+
+  onBrokenLinks: "throw",
+  i18n: {
+    defaultLocale: "en",
+    locales: ["en"],
+  },
+
+  presets: [
+    [
+      "classic",
+      {
+        docs: {
+          sidebarPath: "./sidebars.ts",
+        },
+        theme: {
+          customCss: "./src/css/custom.css",
+        },
+      } satisfies Preset.Options,
+    ],
+  ],
+
+  themeConfig: {
+    colorMode: {
+      respectPrefersColorScheme: true,
+    },
+    navbar: {
+      title: "Dev Reference",
+      logo: {
+        alt: "Dev Reference Logo",
+        src: "img/logo.svg",
+      },
+      items: [
+        {
+          type: "docSidebar",
+          sidebarId: "mainSidebar",
+          position: "left",
+          label: "Docs",
+        },
+        {
+          href: "https://github.com/dileep-hegde/devreference-in.github.io",
+          label: "GitHub",
+          position: "right",
+        },
+      ],
+    },
+    footer: {
+      style: "dark",
+      copyright: `Copyright © ${new Date().getFullYear()} Dev Reference, Inc. Built with Docusaurus.`,
+    },
+    prism: {
+      theme: prismThemes.github,
+      darkTheme: prismThemes.dracula,
+    },
+  } satisfies Preset.ThemeConfig,
+};
+
+export default config;
